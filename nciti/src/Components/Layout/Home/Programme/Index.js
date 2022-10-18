9a2ec8e61cb951557c7d5854694bd3e16c853c87
bigNath500 <nathanjoe.git@gmail.com>
2022-10-18 10:45:22 +0100

    Done with the programme component

--- nciti/src/Components/Layout/Home/Programme/Index.js
+++ nciti/src/Components/Layout/Home/Programme/Index.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Button, Container } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import Programme1 from '../../../Assets/Programme/preveiw_1.png'
+import Programme2 from '../../../Assets/Programme/preveiw_2.png'
+import ProgrammePdf from '../../../Assets/Programme/programme.pdf'
 
 const Index = () => {
   return (
@@ -10,18 +13,30 @@ const Index = () => {
                 <h1 className='text-center text-primary fw-bold text-capitalize'>Programmme of events</h1>
 
                 <div className='d-flex mt-4 justify-content-center align-items-center'>
-                    <div className="bg-white text-center shadow-sm pt-5 pb-5 w-70 p-5 rounded rounded-2">
+                    <div className="bg-white text-center shadow-sm pt-5 pb-5 p-5 rounded rounded-2">
                         <h3 className='text-dark text-center fw-bold'>
-                            Download Programme.
+                            Programme Review.
                         </h3>
 
-                        <p className="mt-4">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit commodi, eaque repudiandae corporis veritatis porro deleniti, at expedita ipsa sunt veniam. Totam nulla unde blanditiis doloremque quisquam labore nisi accusamus.
-                        </p>
+                        <Row>
+                            <Col md={6}>
+                                <div>
+                                    <img src={Programme1} width="100%" alt="nciti-14 programe" />
+                                </div>
+                            </Col>
+
+                            <Col md={6}>
+                                <div>
+                                    <img src={Programme2} width="100%" alt="nciti-14 programe" />
+                                </div>
+                            </Col>
+                        </Row>
 
                         <div className="mt-3 text-center">
-                            <a href="./programme.pdf" download={"Programme"} className='text-decoration-none btn btn-success'>
-                                Download Programme
+                            <h6 className='text-muted'>Download the PDF file of the programme, click the button below.</h6>
+
+                            <a href={ProgrammePdf} download={"Programme"} className='text-decoration-none'>
+                                <Button variant='success'>Download Programme</Button>
                             </a>
                         </div>
                     </div>
