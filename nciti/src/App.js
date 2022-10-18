1539409da5c9d464478b35b7e023a150c30fa2b3
bigNath500 <nathanjoe.git@gmail.com>
2022-10-18 12:17:20 +0100

    Home page completed

--- nciti/src/App.js
+++ nciti/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Components/Layout/Home/Index'
 import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/index'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={ <Home /> } />
         </Routes>
+        <Footer />
       </Router>
     </>
   )
---
+++ nciti/src/Components/Footer/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+const index = () => {
+  return (
+    <>
+        <footer className='bg-dark'>
+          <Container className='text-center text-muted'>
+            <Link className='text-muted text-decoration-none' to={'/'}>
+              About
+            </Link>
+            <span className='ms-2 me-2'>/</span>
+            <Link className='text-muted text-decoration-none' to={'/'}>
+              News
+            </Link>
+            <span className='ms-2 me-2'>/</span>
+            <Link className='text-muted text-decoration-none' to={'/'}>
+              Contact
+            </Link>
+            <span className='ms-2 me-2'>/</span>
+            <Link className='text-muted text-decoration-none' to={'/'}>
+              Registration
+            </Link>
+            <span className='ms-2 me-2'>/</span>
+            <Link className='text-muted text-decoration-none' to={'/'}>
+              Venue
+            </Link>
+            <br /> <br />
+
+            &copy; 2022, NCITI. All rights reserved
+          </Container>
+        </footer>
+    </>
+  )
+}
+
+export default index
